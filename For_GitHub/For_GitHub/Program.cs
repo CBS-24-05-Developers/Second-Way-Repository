302ca50e303859274c9c7945de71b3484bee1e18
Михаил <79160407+MikhailTrofimenko@users.noreply.github.com>
2021-06-16 10:15:14 +0300

    Фиксация нового проекта

---
+++ For_GitHub/For_GitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace For_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            Console.ReadKey();
+ 
+        }
+    }
+}
